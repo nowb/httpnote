e5561885a4675733bc4500f987747b034fa30b86
nowb <nowb@tuta.io>
2021-07-06 23:53:39 +0200

    add unit test for MapRequest func

---
+++ request_info_test.go
@@ -0,0 +1,39 @@
+package httpnote_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"github.com/nowb/httpnote"
+)
+
+func TestMapRequest(t *testing.T) {
+	tests := []struct {
+		r              *http.Request
+		expectedOutput *httpnote.RequestInfo
+	}{
+		{
+			r: httptest.NewRequest(http.MethodGet, "/test", nil),
+			expectedOutput: &httpnote.RequestInfo{
+				Method:     http.MethodGet,
+				URL:        &httpnote.URLInfo{Path: "/test"},
+				Proto:      "HTTP/1.1",
+				ProtoMajor: 1,
+				ProtoMinor: 1,
+				Host:       "example.com",
+				RemoteAddr: "192.0.2.1:1234",
+				RequestURI: "/test",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		output := httpnote.MapRequest(test.r)
+
+		if !reflect.DeepEqual(test.expectedOutput, output) {
+			t.Errorf("expected: %v, got: %v", test.expectedOutput, output)
+		}
+	}
+}

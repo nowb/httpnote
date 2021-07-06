5ba4dda3f9ad8c399f2dc4d4908bfe17b191c6a6
nowb <nowb@tuta.io>
2021-07-07 00:43:47 +0200

    add type that wraps []*FileHeaderInfo, add Zerolog marshalers implementations for all structs

--- request_info.go
+++ request_info.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"mime/multipart"
 	"net/http"
+
+	"github.com/rs/zerolog"
 )
 
 type RequestInfo struct {
@@ -15,7 +17,7 @@ type RequestInfo struct {
 	ProtoMajor       int
 	ProtoMinor       int
 	Header           map[string][]string
-	BodyBytes        []byte
+	Body             []byte
 	BodyBase64       string
 	ContentLength    int64
 	TransferEncoding []string
@@ -31,6 +33,93 @@ type RequestInfo struct {
 	RequestURI       string
 }
 
+func (ri *RequestInfo) MarshalZerologObject(e *zerolog.Event) {
+	e.Str("method", ri.Method)
+
+	if ri.URL != nil {
+		e.Object("url", ri.URL)
+	}
+
+	e.
+		Str("proto", ri.Proto).
+		Int("proto_major", ri.ProtoMajor).
+		Int("proto_minor", ri.ProtoMinor)
+
+	if len(ri.Header) > 0 {
+		header := zerolog.Dict()
+		for k, v := range ri.Header {
+			header.Strs(k, v)
+		}
+
+		e.Dict("header", header)
+	}
+
+	if len(ri.Body) > 0 {
+		e.Bytes("body", ri.Body)
+	}
+
+	if ri.BodyBase64 != "" {
+		e.Str("body_base64", ri.BodyBase64)
+	}
+
+	if ri.ContentLength > 0 {
+		e.Int64("content_length", ri.ContentLength)
+	}
+
+	if len(ri.TransferEncoding) > 0 {
+		e.Strs("transfer_encoding", ri.TransferEncoding)
+	}
+
+	if ri.Close {
+		e.Bool("close", ri.Close)
+	}
+
+	e.Str("host", ri.Host)
+
+	if len(ri.Form) > 0 {
+		form := zerolog.Dict()
+		for k, v := range ri.Form {
+			form.Strs(k, v)
+		}
+
+		e.Dict("form", form)
+	}
+
+	if ri.FormEncoded != "" {
+		e.Str("form_encoded", ri.FormEncoded)
+	}
+
+	if len(ri.PostForm) > 0 {
+		postForm := zerolog.Dict()
+		for k, v := range ri.PostForm {
+			postForm.Strs(k, v)
+		}
+
+		e.Dict("post_form", postForm)
+	}
+
+	if ri.PostFormEncoded != "" {
+		e.Str("post_form_encoded", ri.PostFormEncoded)
+	}
+
+	if ri.MultipartForm != nil {
+		e.Object("multipart_form", ri.MultipartForm)
+	}
+
+	if len(ri.Trailer) > 0 {
+		trailer := zerolog.Dict()
+		for k, v := range ri.Trailer {
+			trailer.Strs(k, v)
+		}
+
+		e.Dict("trailer", trailer)
+	}
+
+	e.
+		Str("remote_addr", ri.RemoteAddr).
+		Str("request_uri", ri.RequestURI)
+}
+
 type URLInfo struct {
 	Scheme      string
 	Opaque      string
@@ -44,23 +133,134 @@ type URLInfo struct {
 	RawFragment string
 }
 
+func (ui *URLInfo) MarshalZerologObject(e *zerolog.Event) {
+	if ui.Scheme != "" {
+		e.Str("scheme", ui.Scheme)
+	}
+
+	if ui.Opaque != "" {
+		e.Str("opaque", ui.Opaque)
+	}
+
+	if ui.User != nil {
+		e.Object("user", ui.User)
+	}
+
+	if ui.Host != "" {
+		e.Str("host", ui.Host)
+	}
+
+	if ui.Path != "" {
+		e.Str("path", ui.Path)
+	}
+
+	if ui.RawPath != "" {
+		e.Str("raw_path", ui.RawPath)
+	}
+
+	if ui.ForceQuery {
+		e.Bool("force_query", ui.ForceQuery)
+	}
+
+	if ui.RawQuery != "" {
+		e.Str("raw_query", ui.RawQuery)
+	}
+
+	if ui.Fragment != "" {
+		e.Str("fragment", ui.Fragment)
+	}
+
+	if ui.RawFragment != "" {
+		e.Str("raw_fragment", ui.RawFragment)
+	}
+}
+
 type UserInfo struct {
 	Username string
 	Password string
 	Encoded  string
 }
 
+func (ui *UserInfo) MarshalZerologObject(e *zerolog.Event) {
+	if ui.Username != "" {
+		e.Str("username", ui.Username)
+	}
+
+	if ui.Password != "" {
+		e.Str("password", ui.Password)
+	}
+
+	if ui.Encoded != "" {
+		e.Str("encoded", ui.Encoded)
+	}
+}
+
 type FormInfo struct {
 	Value map[string][]string
-	File  map[string][]*FileHeaderInfo
+	File  map[string]FileHeaderInfos
+}
+
+type FileHeaderInfos []*FileHeaderInfo
+
+func (fhis FileHeaderInfos) MarshalZerologArray(a *zerolog.Array) {
+	for _, fhi := range fhis {
+		a.Object(fhi)
+	}
+}
+
+func (fi *FormInfo) MarshalZerologObject(e *zerolog.Event) {
+	if len(fi.Value) > 0 {
+		value := zerolog.Dict()
+		for k, v := range fi.Value {
+			value.Strs(k, v)
+		}
+
+		e.Dict("value", value)
+	}
+
+	if len(fi.File) > 0 {
+		file := zerolog.Dict()
+		for k, v := range fi.File {
+			file.Array(k, v)
+		}
+
+		e.Dict("file", file)
+	}
 }
 
 type FileHeaderInfo struct {
 	Filename   string
 	MimeHeader map[string][]string
 	Size       int64
-	FileBytes  []byte
+	File       []byte
 	FileBase64 string
+}
+
+func (fhi *FileHeaderInfo) MarshalZerologObject(e *zerolog.Event) {
+	if fhi.Filename != "" {
+		e.Str("filename", fhi.Filename)
+	}
+
+	if len(fhi.MimeHeader) > 0 {
+		mimeHeader := zerolog.Dict()
+		for k, v := range fhi.MimeHeader {
+			mimeHeader.Strs(k, v)
+		}
+
+		e.Dict("mime_header", mimeHeader)
+	}
+
+	if fhi.Size != 0 {
+		e.Int64("size", fhi.Size)
+	}
+
+	if len(fhi.File) > 0 {
+		e.Bytes("file", fhi.File)
+	}
+
+	if fhi.FileBase64 != "" {
+		e.Str("file_base64", fhi.FileBase64)
+	}
 }
 
 func MapRequest(r *http.Request) *RequestInfo {
@@ -117,10 +317,10 @@ func MapRequest(r *http.Request) *RequestInfo {
 		}
 	}
 
-	// set BodyBytes and BodyBase64
+	// set Body and BodyBase64
 	bodyBytes, err := io.ReadAll(r.Body)
 	if err == nil && len(bodyBytes) > 0 {
-		ri.BodyBytes = bodyBytes
+		ri.Body = bodyBytes
 		ri.BodyBase64 = base64.StdEncoding.EncodeToString(bodyBytes)
 
 		r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
@@ -164,7 +364,7 @@ func MapFileHeader(fh *multipart.FileHeader) *FileHeaderInfo {
 	if err == nil {
 		fileBytes, err := io.ReadAll(file)
 		if err == nil {
-			fhi.FileBytes = fileBytes
+			fhi.File = fileBytes
 			fhi.FileBase64 = base64.StdEncoding.EncodeToString(fileBytes)
 		}
 	}
@@ -184,7 +384,7 @@ func MapForm(f *multipart.Form) *FormInfo {
 	}
 
 	if len(f.File) > 0 {
-		fileMap := make(map[string][]*FileHeaderInfo, len(f.File))
+		fileMap := make(map[string]FileHeaderInfos, len(f.File))
 
 		for k, v := range f.File {
 			var fhiSlice []*FileHeaderInfo
